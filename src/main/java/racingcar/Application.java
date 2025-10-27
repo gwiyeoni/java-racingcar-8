f34d3d93fd5862cffb62042ad00dfa4e5f77500d
gwiyeoni <bcwqcd@gmail.com>
2025-10-27 23:12:37 +0900

    feat : 자동차 이름이 5자 초과일 경우 예외 처리 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,12 +7,22 @@ public class Application {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
+        String[] carNameArr = carName.split(",");
+
+        exceptionCarName(carNameArr);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String count = Console.readLine();
 
-        String[] carNameArr = carName.split(",");
 
-        
+
+    }
+
+    private static void exceptionCarName(String[] carNameArr) {
+        for (String carName : carNameArr) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자를 초과하면 안됩니다");
+            }
+        }
     }
 }
