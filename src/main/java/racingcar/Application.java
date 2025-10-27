96d356dec952af3fdd77e4ae6a6a77e132276de1
gwiyomi <bcwqcd@gmail.com>
2025-10-27 19:09:25 +0900

    feat : 시도할 횟수를 입력받는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,5 +7,9 @@ public class Application {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String count = Console.readLine();
+
     }
 }
